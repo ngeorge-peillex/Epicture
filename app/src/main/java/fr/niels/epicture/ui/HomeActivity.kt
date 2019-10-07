60fb8f5af4cf45cd3d4af58b93a5a798af126eb4
Niels GEORGE-PEILLEX <niels.george-peillex@epitech.eu>
2019-10-07 12:39:38 +0200

    feat: dynamic display pictures

--- app/src/main/java/fr/niels/epicture/ui/HomeActivity.kt
+++ app/src/main/java/fr/niels/epicture/ui/HomeActivity.kt
@@ -1,16 +1,34 @@
 package fr.niels.epicture.ui
 
 import android.os.Bundle
+import android.widget.ImageView
 import fr.niels.epicture.BaseActivity
 import fr.niels.epicture.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity(0) {
+
+    lateinit var newView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
+
+        for (item in pictureArray) {
+
+            newView = ImageView(this)
+
+            myLayout.addView(newView)
+
+            newView.layoutParams.height = 300
+            newView.layoutParams.width = 300
+            newView.x = 5F
+            newView.y = 350F
+            newView.setImageURI(item.link)
+
+        }
 
     }
 }
